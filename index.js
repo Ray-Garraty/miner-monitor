e2eed4e9c92351e67df875caca51f6e58119571e
Ray-Garraty <vl.besed@gmail.com>
2022-01-31 22:37:03 +0300

    added saving data to csv file after timed intervals

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+import fs from 'fs';
 import net from 'net';
 import { convertBufferToObject, extractStatsString, formatStatsString } from './parser.js';
 
 const host = '192.168.1.102';
 const port = 4028;
+const interval = 1000;
+const csvFilePath = 'C:/Users/vlbes/Desktop/log.csv';
 
 const command = 'stats';
 // const command = 'ascset';
 const args = [0, 'fan-spd', '87-100'];
+
+const isFileOpened = async (filepath) => {
+  try {
+    const fileHandle = await fs.promises.open(filepath, fs.constants.O_RDONLY | 0x10000000);
+    fileHandle.close();
+    return false;
+  } catch (error) {
+    if (error.code === 'EBUSY'){
+      return true;
+    } else {
+      throw error;
+    }
+  }
+};
 
 const main = async () => {
   const socket = await net.connect({host, port});
@@ -16,24 +33,32 @@ const main = async () => {
     buffer += data.toString();
   });
   
-  socket.on('end', () => {
+  socket.on('end', async () => {
     const obj = convertBufferToObject(buffer);
     const { STATUS, STATS } = obj;
     const responseCode = STATUS[0].STATUS;
     const responseMessage = STATUS[0].Msg;
     if (responseCode === 'S') {
-      console.log('Успешно');
+      console.log('Получен ответ от майнера');
     } else {
       console.log('Произошла ошибка: ', responseMessage);
-    };
+    }
     if (STATS) {
       const result = extractStatsString(STATS);
-      console.log(result);
-      // console.log(formatStatsString(result));
+      const isOpened = await isFileOpened(csvFilePath);
+      console.log(isOpened);
+      fs.writeFile(csvFilePath, formatStatsString(result), (err) => {
+        if (err) {
+          console.log('Ошибка записи лога в csv-файл: ', err);
+        } else {
+          console.log('Данные сохранены в csv-файл');
+          setTimeout(() => main(), interval);
+        }
+      });
     }
   });
 
   await socket.write(JSON.stringify({ command, parameter: args.join(',')}));
 };
 
-main();
+main().then(setTimeout(() => main(), interval));
