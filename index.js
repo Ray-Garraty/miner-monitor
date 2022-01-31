ee5fcd1df25916557e611410fc06b0e481501049
Ray-Garraty <vl.besed@gmail.com>
2022-01-31 19:22:59 +0300

    removed cgminer-api package

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const Client = require('cgminer-api').client;
+const net = require('net');
+// const Client = require('cgminer-api').client;
 
-const host = '192.168.1.104';
+const host = '192.168.1.102';
 const port = 4028;
-const cgminer = new Client({host, port});
+// const cgminer = new Client({host, port});
 
-const command = {name: 'stats', reply: 'ололо'};
-// const command = {name: 'ascset'};
-const args = [0, 'fans-spd', '86-100'];
+// const command = 'stats';
+const command = 'ascset';
+const args = [0, 'fan-spd', '86-100'];
+
+const parseStatsBuffer = (rawData) => {
+  const object = JSON.parse(rawData
+    .replace(/\-nan/g, '0')
+    .replace(/[^\x00-\x7F]/g, '')
+    .replace(/[^\}]+$/, ''));
+  return object.STATS[0]['MM ID0'];
+};
 
 const main = async () => {
-  await cgminer.connect();
-  await cgminer.request(command, args);
-  // console.log(command);
+  const socket = await net.connect({host, port});
+  let buffer = '';
+  
+  socket.on('data', (data) => {
+    buffer += data.toString();
+  });
+  
+  socket.on('end', () => {
+    console.log(buffer);
+  });
+
+  await socket.write(JSON.stringify({ command, parameter: args.join(',')}));
 };
 
 main();
