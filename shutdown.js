55bd3a7e1b56a04611d6c824d16c54e40b6246a3
Ray-Garraty <vl.besed@gmail.com>
2022-03-04 19:31:10 +0300

    switched to another Yandex Weather API tariff

--- shutdown.js
+++ shutdown.js
@@ -2,7 +2,7 @@ import manage from './manage.js';
 import { port } from './variables.js';
 
 const command = 'ascset';
-const host = '';
+const host = '192.168.1.103';
 const args = [0, 'hashpower', 0];
 
 manage(host, port, command, args);
--- weather.js
+++ weather.js
@@ -9,7 +9,7 @@ const longitude = process.env.longitude;
 const folderPath = process.env.LOCAL_GDRIVE_PATH;
 const weatherFilePath = `${folderPath}weather.txt`;
 
-const saveWeather = () => axios.get(`https://api.weather.yandex.ru/v2/forecast?lat=${latitude}&lon=${longitude}`, {
+const saveWeather = () => axios.get(`https://api.weather.yandex.ru/v2/informers?lat=${latitude}&lon=${longitude}`, {
     headers: { 'X-Yandex-API-Key': apiKey }
   })
   .then((response) => {
@@ -21,7 +21,7 @@ const saveWeather = () => axios.get(`https://api.weather.yandex.ru/v2/forecast?l
     });
   })
   .catch((error) => {
-    console.log('\nОшибка при запросе погоды с Яндекса: ', error);
+    console.log('\nОшибка при запросе погоды с Яндекса: ', error.message);
   })
   .finally(() => {
     setTimeout(() => saveWeather(), weatherUpdateInterval);
