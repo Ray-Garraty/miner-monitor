c90c11412191008bdff6e0b69324e50d8f9551d6
Ray Garraty <vl.besed@gmail.com>
2022-03-05 16:40:24 +0300

    added 1 more miner IP and changed weather request timeout

--- setfans.js
+++ setfans.js
@@ -2,7 +2,7 @@ import manage from './manage.js';
 import { port } from './variables.js';
 
 const command = 'ascset';
-const host = '';
-const args = [0, 'fan-spd', '46-100'];
+const host = '192.168.1.7';
+const args = [0, 'fan-spd', '41-100'];
 
 manage(host, port, command, args);
--- variables.js
+++ variables.js
@@ -1,7 +1,8 @@
 export const hostsIPs = [
+  '192.168.1.6',
   '192.168.1.7',
   '192.168.1.8',
 ];
 export const port = 4028;
 export const updateInterval = 1800000;
-export const weatherUpdateInterval = 28800000;
+export const weatherUpdateInterval = 1800000;
