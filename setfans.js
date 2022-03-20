7637d8448b0b3681d38b55580199cacae0b0f8b1
Ray Garraty <vl.besed@gmail.com>
2022-03-20 09:36:04 +0300

    added switchmodes script

--- setfans.js
+++ setfans.js
@@ -2,7 +2,7 @@ import manage from './manage.js';
 import { port } from './variables.js';
 
 const command = 'ascset';
-const host = '192.168.1.7';
-const args = [0, 'fan-spd', '41-100'];
+const host = '192.168.1.6';
+const args = [0, 'fan-spd', '43-100'];
 
 manage(host, port, command, args);
