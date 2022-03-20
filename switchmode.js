62238c26325505bc4e90adbc74c52e064969b530
Ray Garraty <vl.besed@gmail.com>
2022-03-20 09:46:41 +0300

    added reboot script

---
+++ switchmode.js
@@ -0,0 +1,8 @@
+import manage from './manage.js';
+import { port } from './variables.js';
+
+const command = 'ascset';
+const host = '192.168.1.6';
+const args = [0, 'workmode', 1];
+
+manage(host, port, command, args);
